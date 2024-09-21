97d7832efc83fa59a73939ca6d97d89863b930e2
Maria <globalkmaria@gmail.com>
2024-09-21 23:06:08 +0100

    feat : add s3 feature

---
+++ src/config/index.ts
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+const required = (key: string, defaultValue = undefined) => {
+  const value = process.env[key] || defaultValue;
+  if (value == null) {
+    throw new Error(`Key ${key} is undefined`);
+  }
+  return value;
+};
+
+export const config = {
+  session: {
+    secret: required("SESSION_SECRET"),
+  },
+  server: {
+    url: required("SERVER_URL"),
+  },
+  port: parseInt(required("PORT")),
+  cors: {
+    allowedOrigin: required("CORS_ALLOW_ORIGIN"),
+  },
+  frontend: {
+    url: required("FRONTEND_URL"),
+  },
+  mysql: {
+    host: required("MYSQL_HOST"),
+    user: required("MYSQL_USER"),
+    password: required("MYSQL_PASSWORD"),
+    database: required("MYSQL_NAME"),
+    port: required("MYSQL_PORT"),
+  },
+  s3: {
+    accessKeyId: required("AWS_ACCESS_KEY"),
+    secretAccessKey: required("AWS_SECRET_KEY"),
+    region: required("AWS_REGION"),
+    bucket: required("AWS_BUCKET"),
+    s3ForcePathStyle: true,
+  },
+};
+
+export const corsOption = {
+  origin: config.cors.allowedOrigin,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+const sessionOptions = {
+  secret: config.session.secret,
+  resave: false,
+  saveUninitialized: false,
+};
+
+export const getSessions = () => sessionOptions;
---
+++ src/db/aws.ts
@@ -0,0 +1,27 @@
+import { config } from "../config/index";
+import { S3Client } from "@aws-sdk/client-s3"; // Import AWS SDK v3 S3 client
+import multer from "multer"; // Import Multer for file uploads
+import multerS3 from "multer-s3"; // Import Multer S3 storage engine
+
+// Create an S3 client instance
+export const s3Client = new S3Client({
+  region: config.s3.region,
+  credentials: {
+    accessKeyId: config.s3.accessKeyId,
+    secretAccessKey: config.s3.secretAccessKey,
+  },
+});
+
+// Configure Multer for file uploads to S3
+export const upload = multer({
+  storage: multerS3({
+    s3: s3Client,
+    bucket: config.s3.bucket,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+});
