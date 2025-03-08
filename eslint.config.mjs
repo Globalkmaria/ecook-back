411f4e8f98705d9b1b50ff42d38e5ddbb17fcbb0
Song Hyun Kim <globalkmaria@gmail.com>
2025-03-08 13:19:12 +0900

    feat: add eslint import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -8,6 +9,7 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  { plugins: { import: importPlugin } },
   {
     rules: {
       "@typescript-eslint/no-unused-expressions": "off",
@@ -17,6 +19,24 @@ export default [
         { argsIgnorePattern: "^_" },
       ],
       "@typescript-eslint/explicit-module-boundary-types": "off",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "import/no-duplicates": "error",
+      "import/newline-after-import": ["error", { count: 1 }],
+      "import/no-default-export": "off",
     },
   },
 ];
