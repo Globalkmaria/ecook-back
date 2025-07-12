baa92b48a75efa8535fd6df31bae7c23aecd2fbf
SongHyun Kim <globalkmaria@gmail.com>
2025-07-13 05:17:23 +0900

    fix : validate user in home recipe API to prevent showing deleted users (#13)
    * fix :  validate user in home recipe API to prevent showing deleted users
    
    * chore: bump version to 1.4.1 in package.json

--- src/helpers/checkUser.ts
+++ src/helpers/checkUser.ts
@@ -1,5 +1,8 @@
 import { User } from "@/services/recipes/recipe/type";
+import { RecipesSimple } from "@/services/recipes/type";
 import { getImgUrl } from "@/utils/img";
+
+const DELETED_ACCOUNT_USERNAME = "anonymous_chef";
 
 const isDeletedAccountUser = (user: Pick<User, "deleted_at">) => {
   return user.deleted_at !== null;
@@ -11,7 +14,17 @@ export const getValidSimpleUser = (
   const isDeleted = isDeletedAccountUser(user);
   return {
     isDeleted,
-    username: isDeleted ? "anonymous_chef" : user.username,
+    username: isDeleted ? DELETED_ACCOUNT_USERNAME : user.username,
     img: getImgUrl(user.img, true),
+  };
+};
+
+export const getValidUser = (
+  data: Pick<RecipesSimple, "user_username" | "user_deleted_at">
+) => {
+  const isDeleted = data.user_deleted_at !== null;
+  return {
+    username: isDeleted ? DELETED_ACCOUNT_USERNAME : data.user_username,
+    isDeleted,
   };
 };
